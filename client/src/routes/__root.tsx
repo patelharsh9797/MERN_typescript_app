578f80e1bf0d6b651b7afa69e5e4cd909ad3bb3f
Harsh Patel <patelharsh9797@gmail.com>
2025-09-25 12:53:23 +0000

    client: tailwindcss

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -8,11 +8,11 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <React.Fragment>
-      <div>Hello "__root"!</div>
-      <div>
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <div className="flex underline gap-4">
         <Link to="/" className="[&.active]:font-bold">
           Home
-        </Link>{" "}
+        </Link>
         <Link to="/" className="[&.active]:font-bold">
           About
         </Link>
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,21 +1,9 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return (
-    <div>
-      Hello "/"!
-      <div>
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/" className="[&.active]:font-bold">
-          About
-        </Link>
-      </div>
-    </div>
-  );
+  return <div className="text-3xl text-teal-700 font-bold">Hello "/"!</div>;
 }
